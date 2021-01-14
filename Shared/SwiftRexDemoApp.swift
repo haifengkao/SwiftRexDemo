a22372876cb3284daee255af0f08b14a5e4bdc56
Hai Feng Kao <haifeng@cocoaspice.in>
2021-01-14 13:21:09 +0800

    Initial Commit

---
+++ Shared/SwiftRexDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRexDemoApp.swift
+//  Shared
+//
+//  Created by Lono on 2021/1/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRexDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
